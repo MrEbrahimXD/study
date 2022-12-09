03bf5885d2dfe887ef2121f3f7c74764eeb83c18
MrEbrahimXD <mrtankgt@outlook.com>
2022-12-09 05:06:00 +0200

    Added more Nasheeds / Fixed Names

--- canvas.js
+++ canvas.js
@@ -3,7 +3,7 @@ const intro = document.getElementById("swup");
 const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
 function rfAudio() {
-    let num = random(1,7);
+    let num = random(1,14);
     document.getElementsByClassName('audio')[num].play();
     document.getElementsByClassName('audio')[num].addEventListener('ended',function() {
     });
