a633d0548dc160f45a9b9f23184e6cd7afe09eac
MrEbrahimXD <mrtankgt@outlook.com>
2022-12-13 07:33:06 +0200

    New Header and New Features / Bug fixes

--- canvas.js
+++ canvas.js
@@ -1,10 +1,20 @@
-const intro = document.getElementById("swup");
-
+const nasheeds = document.getElementsByClassName('audio');
 const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+const canvas = document.getElementById('canvas');
 
 function rfAudio() {
+    for (let a = 0; a < nasheeds.length; a++ ) {
+        nasheeds[a].pause();
+        nasheeds[a].currentTime = 0;
+    }
+    canvas.style.display = "block";
+    
     let num = random(1,14);
     document.getElementsByClassName('audio')[num].play();
     document.getElementsByClassName('audio')[num].addEventListener('ended',function() {
+        canvas.style.display = "none";
     });
 }
+
+
+
--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-$(document).ready( function() {
-  $(".nav-button").click( function() {
-    $('')
-    $(this).toggleClass("open");
-    $('.ul').toggleClass("open");
-    $('.nav-ul').toggleClass("open");
-  });
-});
+
 
 // const important = document.querySelector('.important');
 
