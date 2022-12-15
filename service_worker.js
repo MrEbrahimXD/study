edca3fd82b0b8e82fc44acea35a289a5e003be75
Ebrahim Ahmed <87176403+MrEbrahimXD@users.noreply.github.com>
2022-12-15 13:27:19 +0200

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,6 +1,18 @@
-importScripts("https://cdn.jsdelivr.net/npm/workbox-cdn/workbox/workbox-sw.js")
+// This is the "Offline copy of pages" service worker
+
+const CACHE = "pwabuilder-offline";
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
 
 workbox.routing.registerRoute(
-    ({request}) => request.destination === "image",
-    new workbox.strategies.NetworkFirst()
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
 );
